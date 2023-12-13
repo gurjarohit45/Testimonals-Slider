4f6a7ccd709fc65735c5776b4823404fd148e664
Rohit <gujjarohit450@gmail.com>
2023-12-13 16:40:45 +0530

    testimonal

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Rohit Gurjar",
+    job: "MERN Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/93965871?s=96&v=4",
+    text: "I'm a Frontend Web Developer, I mastered HTML, and CSS, and am a beginner in JavaScript, and Tailwind CSS. I can convert your design into pixel perfect frontend website. I provide well-structured, valid, adaptive, featured, clean, readable, and maintainable code.",
+  },
+  {
+    id: 2,
+    name: "Sujeet Kumar Pal",
+    job: "Graphic Designer",
+    image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Kapil Suara",
+    job: "Marketing Manager",
+    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Sahil Sahu",
+    job: "Content Strategist",
+    image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Sandeep Joshi",
+    job: "Data Scientist",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 6,
+    name: "Gourav Choudhary",
+    job: "Software Engineer",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 7,
+    name: "Kunal Marmat",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 8,
+    name: "Bhawani Singh",
+    job: "IAS",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 9,
+    name: "Shishir",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 10,
+    name: "Harshit Chopde",
+    job: "MERN Stack Developer",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I'm a Frontend Web Developer, I mastered HTML, and CSS, and am a beginner in JavaScript, and Tailwind CSS. I can convert your design into pixel perfect frontend website. I provide well-structured, valid, adaptive, featured, clean, readable, and maintainable code.",
+  },
+];
+
+export default reviews;
